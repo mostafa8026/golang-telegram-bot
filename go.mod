27ea35160ac37d6ae37efe0b89f69d861fcf7327
mostafa H <mostafaheidary@hotmail.com>
2020-07-03 16:14:46 +0430

    refactor: 💡 Start creating the bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mostafa8026/golang-telegram-bot
+
+go 1.14
+
+require gopkg.in/tucnak/telebot.v2 v2.3.2 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+var bot *tb.Bot
+
+func main() {
+	fmt.Println("Make the bot exist. :)")
+
+}
