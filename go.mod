f5f3eb9bde191ee4111b3662c606e5013c97bc29
mostafa H <mostafaheidary@hotmail.com>
2020-07-03 18:59:17 +0430

    feat: 🎸 Send message successfully

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mostafa8026/golang-telegram-bot
 
 go 1.14
 
-require gopkg.in/tucnak/telebot.v2 v2.3.2 // indirect
+require (
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"golang.org/x/net/proxy"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
@@ -11,4 +18,51 @@ var bot *tb.Bot
 func main() {
 	fmt.Println("Make the bot exist. :)")
 
+	var httpClient *http.Client
+	httpClient, err := initSocks5Client()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Start creating bot ...")
+	bot, err := tb.NewBot(tb.Settings{
+		Token:  "1307117789:AAFqYg88j-R4dykmjY_J0FxAHKG8Cf306is",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+		Client: httpClient,
+	})
+	if err != nil {
+		log.Fatalf("Cannot start bot. Error: %v\n", err)
+	}
+
+	bot.Handle("/start", func(m *tb.Message) {
+		msg := "Testing the bot"
+		if _, err = bot.Send(m.Chat, msg); err != nil {
+			log.Println(err)
+		}
+		log.Println("Start request has been sent by: %v\n in chat: %v", m.Sender, m.Chat)
+	})
+
+	log.Println("Bot started")
+	go func() {
+		bot.Start()
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+	log.Println("Shutdown signal received, exiting...")
+}
+
+func initSocks5Client() (*http.Client, error) {
+	addr := fmt.Sprintf("%s:%s", "127.0.0.1", "9350")
+	dialer, err := proxy.SOCKS5("tcp", addr, &proxy.Auth{User: " ", Password: " "}, proxy.Direct)
+	if err != nil {
+		return nil, fmt.Errorf("cannot init socks5 proxy client dialer: %w", err)
+	}
+
+	httpTransport := &http.Transport{}
+	httpClient := &http.Client{Transport: httpTransport}
+	httpTransport.Dial = dialer.Dial
+
+	return httpClient, nil
 }
